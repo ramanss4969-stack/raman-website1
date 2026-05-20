0fc9c7f0d08ad685035dda9d29923de32ad4d0d2
ramanss4969-stack <ramanss4969@gmail.com>
2026-05-20 10:47:00 -0700

    Format price in listings.js with commas

--- listings.js
+++ listings.js
@@ -2,7 +2,7 @@ window.propertyListings = [
   {
     status: "Active",
     title: "Family home in Surrey",
-    price: "1499888",
+    price: "1,499,888",
     details: "Surrey | 6 beds | 5 baths",
     description: "Strong rental income potential -Includes rare 1-bedroom detached legal coach house with full kitchen and laundry -Also features a 2-bedroom basement suite with separate entry, full kitchen, and laundry -Total of 3 separate washer/dryer units -Spacious, well-appointed main home -Situated on one of the largest lots in the area (nearly 4,000 sq ft) (31844211)",
     image: "surrey listing.jpg", // Fixed path
